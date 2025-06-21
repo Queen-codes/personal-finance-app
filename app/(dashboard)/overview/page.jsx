a0967f19eed57b2803b075e60198cf4102241ab7
Queen <fawalequeen97@gmail.com>
2025-06-21 18:08:32 +0100

    removed max-width for responsiveness issues on large screems

--- app/(dashboard)/overview/page.jsx
+++ app/(dashboard)/overview/page.jsx
@@ -6,7 +6,7 @@ import Pots from "../../ui/overview/Pots";
 
 export default function Page() {
   return (
-    <div className="mr-[1rem] ml-[1rem] max-w-6xl mx-auto">
+    <div className="mr-[1rem] ml-[1rem] mx-auto">
       <h1 className="text-grey-900 text-3xl font-bold tracking-wide mb-7 mt-8">
         Overview
       </h1>
