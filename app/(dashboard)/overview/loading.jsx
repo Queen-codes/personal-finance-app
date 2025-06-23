9128076377cbe7c4ea9f646f2527978abd320052
Queen <fawalequeen97@gmail.com>
2025-06-23 08:36:11 +0100

    updated loading file

---
+++ app/(dashboard)/overview/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center", fontSize: "1.2em" }}>
+      Loading...
+      <p>Please wait while we fetch the data.</p>
+    </div>
+  );
+}
